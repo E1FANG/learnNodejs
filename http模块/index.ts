5acf2064f28f30a1b9eaa46b21832cf746633f05
e1fang <438283787@qq.com>
2020-10-31 16:46:34 +0800

    使用url.parse得到url基本路径

--- http模块/index.ts
+++ http模块/index.ts
@@ -2,6 +2,7 @@ import * as http from "http";
 import { IncomingMessage, ServerResponse } from "http";
 import * as fs from "fs";
 import * as p from "path";
+import * as url from "url";
 // __dirname 表示当前文件所在目录
 
 const server = http.createServer();
@@ -9,8 +10,9 @@ const publicDir = p.resolve(__dirname, "public");
 // path.resolve() 传入路径或路径片段，解析为绝对路径
 
 server.on("request", (request: IncomingMessage, response: ServerResponse) => {
-  const { method, url, headers } = request;
-  switch (url) {
+  const { method, url: path, headers } = request;
+  const { pathname, search } = url.parse(path);
+  switch (pathname) {
     case "/index.html":
       response.setHeader("Content-Type", "text/html; chartset=utf-8");
       fs.readFile(p.resolve(publicDir, "index.html"), (error, data) => {
@@ -32,6 +34,9 @@ server.on("request", (request: IncomingMessage, response: ServerResponse) => {
         response.end(data.toString());
       });
       break;
+    default:
+      response.statusCode = 404;
+      response.end();
   }
 });
 
