15f0d9a894f3adb44901104988801de8728c593d
e1fang <438283787@qq.com>
2020-11-01 21:36:00 +0800

    匹配任意文件

--- http模块/index.ts
+++ http模块/index.ts
@@ -12,32 +12,18 @@ const publicDir = p.resolve(__dirname, "public");
 server.on("request", (request: IncomingMessage, response: ServerResponse) => {
   const { method, url: path, headers } = request;
   const { pathname, search } = url.parse(path);
-  switch (pathname) {
-    case "/index.html":
-      response.setHeader("Content-Type", "text/html; chartset=utf-8");
-      fs.readFile(p.resolve(publicDir, "index.html"), (error, data) => {
-        if (error) throw error;
-        response.end(data.toString());
-      });
-      break;
-    case "/style.css":
-      response.setHeader("Content-Type", "text/css; charset=utf-8");
-      fs.readFile(p.resolve(publicDir, "style.css"), (error, data) => {
-        if (error) throw error;
-        response.end(data.toString());
-      });
-      break;
-    case "/main.js":
-      response.setHeader("Content-Type", "text/javascript; charset=utf-8");
-      fs.readFile(p.resolve(publicDir, "main.js"), (error, data) => {
-        if (error) throw error;
-        response.end(data.toString());
-      });
-      break;
-    default:
+  console.log(url.parse(path));
+  // response.setHeader("Content-Type", "text/html; chartset=utf-8");
+  // /index.html => index.html
+  const fileName = pathname.substr(1);
+  fs.readFile(p.resolve(publicDir, fileName), (error, data) => {
+    if (error) {
       response.statusCode = 404;
-      response.end();
-  }
+      response.end(`你要的文件不存在`);
+    } else {
+      response.end(data.toString());
+    }
+  });
 });
 
 server.listen(8888);
